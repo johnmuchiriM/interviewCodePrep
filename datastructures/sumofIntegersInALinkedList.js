14788f4f240db6e47145909da65f510ffc48b90b
johnmuchiriM <jeanfrancaiseharicot@gmail.com>
2022-10-21 05:23:26 -0700

    update: added the recursive version of the sum of the numbers in a Linkedlist

--- datastructures/sumofIntegersInALinkedList.js
+++ datastructures/sumofIntegersInALinkedList.js
@@ -14,17 +14,25 @@ a.next = b;
 b.next = c;
 c.next = d;
 
-const sumList = (head) =>{
-    let sum = 0;
-    let current = head;
 
-    while(current !== null){
-        sum += current.val;
-        current = current.next;
-    }
-    console.log(sum);
-    return sum;
+// Iterative
+// const sumList = (head) =>{
+//     let sum = 0;
+//     let current = head;
+
+//     while(current !== null){
+//         sum += current.val;
+//         current = current.next;
+//     }
+//     console.log(sum);
+//     return sum;
     
+// }
+
+//Recursive
+const sumList = (head) => {
+    if(head === null) return 0;
+    return head.val + sumList(head.next);
 }
 
 sumList(a);
