b7fd1c64998c0e436ff0a4d62695f54c3b2bffef
johnmuchiriM <60730009+johnmuchiriM@users.noreply.github.com>
2023-03-09 09:01:34 +0300

    Update reverseList.js

--- datastructures/reverseList.js
+++ datastructures/reverseList.js
@@ -14,34 +14,34 @@ a.next = b;
 b.next = c;
 c.next = d;
 
-// Iterative | O(n) time and O(1) space complexity
-// const reverseList = (head) => {
-//     let prev = null; //2nd
-//     let current = head; //1st
+Iterative | O(n) time and O(1) space complexity
+const reverseList = (head) => {
+    let prev = null; //2nd
+    let current = head; //1st
 
-//     while(current !== null){
-//         const next = current.next; //1st to keep track of the next variable before overidding it
-//         current.next = prev; //2nd :Having the arrow point to the previous node | This would override without the declaration above
-//         prev = current; // 4th: The prev shou
-//         current = next; //3rd 
-//         console.log(prev.val);
+    while(current !== null){
+        const next = current.next; //1st to keep track of the next variable before overidding it
+        current.next = prev; //2nd :Having the arrow point to the previous node | This would override without the declaration above
+        prev = current; // 4th: The prev shou
+        current = next; //3rd 
+        console.log(prev.val);
 
-//     }
+    }
     
-//     return prev; // The previous will become the new head, it was the tail
-// }
-
-// Recursive O(n) and O(n) space and time complecities
-const reverseList = (head, prev = null) => {
-    if(head === null) return prev;
-    const next = head.next;
-    head.next = prev;
-    console.log(prev);
-    return reverseList(next, head);
-    
+    return prev; // The previous will become the new head, it was the tail
 }
 
-reverseList(a,null);
+// Recursive O(n) and O(n) space and time complecities
+// const reverseList = (head, prev = null) => {
+//     if(head === null) return prev;
+//     const next = head.next;
+//     head.next = prev;
+//     console.log(prev);
+//     return reverseList(next, head);
+    
+// }
+
+// reverseList(a,null);
 
 // N  <-  a   b   c  d
 //      prev cur next
