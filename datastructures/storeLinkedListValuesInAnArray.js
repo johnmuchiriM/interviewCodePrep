1da940bd74dd24c4a9ff323ee00654a1dd2b2c4d
johnmuchiriM <60730009+johnmuchiriM@users.noreply.github.com>
2023-03-16 18:33:00 +0300

    Update storeLinkedListValuesInAnArray.js

--- datastructures/storeLinkedListValuesInAnArray.js
+++ datastructures/storeLinkedListValuesInAnArray.js
@@ -16,15 +16,15 @@ c.next = d;
 
 
 // ITERATIVELY
-// const linkedListValues = (head) => {
-//     const values = [];
-//     let current = head;
-//     while(current !== null) {
-//         values.push(current.val);
-//         current = current.next;
+const linkedListValues = (head) => {
+    const values = [];
+    let current = head;
+    while(current !== null) {
+        values.push(current.val);
+        current = current.next;
         
 
-//     }
+    }
     
 
 // }
