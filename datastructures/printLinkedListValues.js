3df82ff641497b731fb0eb652d192d4400b426de
johnmuchiriM <60730009+johnmuchiriM@users.noreply.github.com>
2023-03-16 17:45:56 +0300

    Update printLinkedListValues.js

--- datastructures/printLinkedListValues.js
+++ datastructures/printLinkedListValues.js
@@ -17,17 +17,17 @@ c.next = d;
 // A -> B -> C -> D -> Null
 // cur
 
-// Iterative version of LinkedList
-// const printLinkedList = (head) => {
-//     let current = head;
-//     while( current !== null){
-//         console.log(current.val);
-//         current = current.next;
-//     }
+Iterative version of LinkedList
+const printLinkedList = (head) => {
+    let current = head;
+    while( current !== null){
+        console.log(current.val);
+        current = current.next;
+    }
 
    
 
-// }
+}
 
 //Recursive version of LinkedList Javascript
 // For the recursive nature of the problem the function calls itself 
